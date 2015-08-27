66237e5b635f262d5f502851f87528a8138b5f28
Jackie Kay <jackie@osrfoundation.org>
2015-08-27 15:28:45 -0700

    Comment QoS demo

--- image_tools/include/image_tools/options.hpp
+++ image_tools/include/image_tools/options.hpp
@@ -20,12 +20,34 @@
 
 #include <rmw/types.h>
 
+/// Find "option" in the argument vector.
+/**
+ * \param[in] args The argument vector
+ * \param[in] option The option to search for
+ * \return True if option was found in args, false otherwise.
+ */
 bool find_command_option(
   const std::vector<std::string> & args, const std::string & option);
 
+/// Get the value corresponding to option.
+/**
+ * \param[in] args The argument vector to search in
+ * \param[in] option The option to search for
+ * \return The value that comes after "option"
+ */
 std::string get_command_option(
   const std::vector<std::string> & args, const std::string & option);
 
+/// Parse the C-style argument vector and return demo-specific parameters.
+/**
+ * \param[in] argc Size of the argument vector.
+ * \param[in] argv Argument vector, an array of C-style strings.
+ * \param[in] depth The queue size for the KEEP_LAST QoS policy.
+ * \param[in] reliability_policy The reliability policy (RELIABLE or BEST_EFFORT).
+ * \param[in] show_camera True to show the input stream, false or NULL otherwise.
+ * \param[in] width The width of the image to get, 320 by default.
+ * \param[in] height The height of the image to get, 240 by default.
+ */
 bool parse_command_options(
   int argc, char ** argv, size_t * depth,
   rmw_qos_reliability_policy_t * reliability_policy,
