4f7225a750af3756c2006fa93efe077545165c2b
William Woodall <william@osrfoundation.org>
2016-09-07 11:26:40 -0700

    missing Windows include

--- image_tools/src/burger.cpp
+++ image_tools/src/burger.cpp
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#ifdef _WIN32
+#include <Windows.h>  // For GetTickCount().
+#endif
+
 #include <cstring>
 #include <cstdio>
 #include <vector>
