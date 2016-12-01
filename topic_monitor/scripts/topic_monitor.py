40d973700ce70ebf769a9f3396a4b65c8366fe9a
dhood <dhood@users.noreply.github.com>
2016-11-30 16:35:37 -0800

    Refactor and improve comments

--- topic_monitor/scripts/topic_monitor.py
+++ topic_monitor/scripts/topic_monitor.py
@@ -16,26 +16,27 @@
 
 import argparse
 import re
-import sys
 from threading import Lock, Thread
 import time
 
 import rclpy
-from rclpy.qos import qos_profile_default, qos_profile_sensor_data, QoSReliabilityPolicy
-
-import matplotlib.pyplot as plt
+from rclpy.qos import qos_profile_default, qos_profile_sensor_data
 
 from std_msgs.msg import Float32, Int64
 
-DISPLAY_DASHBOARD = False
+WINDOW_SIZE = 20
 
-time_between_msgs = 0.3  # time in seconds between expected messages
-stale_time = 3.0*time_between_msgs  # time in seconds after which a message is considered stale
-time_between_rate_calculations = 1  # time in seconds between analysis of reception rates
-allowed_latency = 10  # allowed time in seconds between expected and received messages
+time_between_msgs = 0.3               # time in seconds between expected messages
+stale_time = 3.0 * time_between_msgs  # time in seconds after which a message is considered stale
+time_between_rate_calculations = 1    # time in seconds between analysis of reception rates
+allowed_latency = 10                  # allowed time in seconds between expected and received msgs
+
+monitored_topics_lock = Lock()
 
 
 class MonitoredTopic:
+    """Monitor for the reception rate and status of a single topic."""
+
     def __init__(self, topic_id):
         self.topic_id = topic_id
         self.status = 'Offline'
@@ -74,14 +75,14 @@ class MonitoredTopic:
             self.received_values.append(received_value)
         self.time_of_last_data = time.time()  # TODO: time stamp of msg
         status_changed = status != self.status
-        self.status_changed |= status_changed  # don't clear the flag before check_status
+        self.status_changed |= status_changed  # don't clear the flag before check_status is called
         self.status = status
         monitored_topics_lock.release()
 
     def check_status(self, current_time=time.time()):
         if self.status != 'Offline':
-            elapsed_time = current_time - self.time_of_last_data 
-            if elapsed_time > stale_time*1.1:
+            elapsed_time = current_time - self.time_of_last_data
+            if elapsed_time > stale_time * 1.1:
                 self.status_changed = self.status != 'Stale'
                 self.status = 'Stale'
 
@@ -101,11 +102,12 @@ class MonitoredTopic:
         return rate
 
 
-
 class TopicMonitor:
+    """Monitor of a set of topics that match a specified topic name pattern."""
+
     def __init__(self):
+        self.data_topic_pattern = re.compile("((?P<data_name>\w*)_data_?(?P<reliability>\w*))")
         self.status_changed = False
-        self.data_topic_pattern = re.compile("((\w*)_data_?(\w*))")
         self.monitored_topics = {}
         self.publishers = {}
         self.reception_rate_topic_name = 'reception_rate'
@@ -121,7 +123,7 @@ class TopicMonitor:
             monitored_topic.topic_data_callback,
             qos_profile)
 
-        # Create a timer for maintaining the expected value received from the topic
+        # Create a timer for maintaining the expected value received on the topic
         timer_for_allowed_latency = node.create_timer(
             allowed_latency, monitored_topic.timer_for_allowed_latency_callback)
         timer_for_allowed_latency.cancel()
@@ -131,13 +133,14 @@ class TopicMonitor:
         monitored_topic.timer_for_incrementing_expected_value = timer
         monitored_topic.timer_for_allowed_latency = timer_for_allowed_latency
         self.monitored_topics[topic_name] = monitored_topic
-        monitored_topics_lock.release()
 
         # Create a publisher for the reception rate of the topic
         self.publishers[topic_name] = node.create_publisher(
             Float32, self.reception_rate_topic_name + '/' + topic_name)
+        monitored_topics_lock.release()
 
     def get_topic_info(self, topic_name):
+        """Infer topic info (e.g. QoS reliability) from a topic name."""
         match = re.search(self.data_topic_pattern, topic_name)
         if match and match.groups():
             if match.groups()[0] != topic_name:
@@ -146,10 +149,10 @@ class TopicMonitor:
 
             topic_info = {'reliability': 'reliable'}
             topic_info['topic_name'] = topic_name
-            topic_info['data_name'] = match.groups()[1]
+            topic_info['data_name'] = match.group('data_name')
 
-            reliability = match.groups()[2]
-            if reliability == 'best_effort': 
+            reliability = match.group('reliability')
+            if reliability == 'best_effort':
                 topic_info['reliability'] = 'best_effort'
             return topic_info
 
@@ -178,22 +181,18 @@ class TopicMonitor:
         return status_changed
 
     def calculate_reception_rates(self):
-        status_changed = self.check_status()
         monitored_topics_lock.acquire()
         for topic_id, monitored_topic in self.monitored_topics.items():
             rate = monitored_topic.current_reception_rate()
             monitored_topic.reception_rate_over_time.append(rate)
             rateMsg = Float32()
             rateMsg.data = rate
-            #TODO add lock here
-            try:
-                self.publishers[topic_id].publish(rateMsg)
-            except KeyError:
-                pass
+            self.publishers[topic_id].publish(rateMsg)
         monitored_topics_lock.release()
 
 
-class TopicMonitorDashboard:
+class TopicMonitorDisplay:
+    """Display of the monitored topic reception rates."""
 
     def __init__(self, topic_monitor):
         self.topic_monitor = topic_monitor
@@ -219,15 +218,14 @@ class TopicMonitorDashboard:
         # Shrink axis' height to make room for legend
         shrink_amnt = 0.2
         box = self.ax.get_position()
-        self.ax.set_position([box.x0, box.y0 + box.height * shrink_amnt,
-            box.width, box.height * (1 - shrink_amnt)])
-
+        self.ax.set_position(
+            [box.x0, box.y0 + box.height * shrink_amnt, box.width, box.height * (1 - shrink_amnt)])
 
     def add_monitored_topic(self, topic_id):
         # Make first instance of the line so that we only have to update it later
         y_data = [None] * self.x_range
-        line, = self.ax.plot(self.x_data, y_data,
-            '-', color=self.colors[self.topic_count % len(self.colors)],
+        line, = self.ax.plot(
+            self.x_data, y_data, '-', color=self.colors[self.topic_count % len(self.colors)],
             marker=self.markers[self.topic_count % len(self.markers)], label=topic_id)
         self.reception_rate_plots[topic_id] = line
         self.ax.plot(self.x_data, [None] * self.x_range)
@@ -238,13 +236,13 @@ class TopicMonitorDashboard:
             self.ax.xaxis.set_ticklabels(labels)
 
         # Update the plot legend to include the new line
-        self.ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1),
-        fancybox=True, shadow=True, ncol=2)
+        self.ax.legend(
+            loc='upper center', bbox_to_anchor=(0.5, -0.1), fancybox=True, shadow=True, ncol=2)
 
         self.topic_count += 1
         self.monitored_topics.append(topic_id)
 
-    def update_dashboard(self):
+    def update_display(self):
         monitored_topics_lock.acquire()
         for topic_id, monitored_topic in self.topic_monitor.monitored_topics.items():
             topic_id = monitored_topic.topic_name
@@ -259,53 +257,67 @@ class TopicMonitorDashboard:
 
         self.fig.canvas.draw()
 
-topic_monitor = TopicMonitor()
-monitored_topics_lock = Lock()
-WINDOW_SIZE = 20
 
-class RCLPYThread(Thread):
-    def __init__(self):
-        rclpy.init()
+class DataReceivingThread(Thread):
+    def __init__(self, topic_monitor):
+        self.topic_monitor = topic_monitor
         Thread.__init__(self)
 
     def run(self):
         self.node = rclpy.create_node('topic_monitor')
-        while rclpy.ok():
-            # Check if there is a new topic online
-            # TODO: use graph events
-            topic_names_and_types = self.node.get_topic_names_and_types()
-            for topic_name in topic_names_and_types.topic_names:
-                # Infer the appropriate QoS profile from the topic name
-                topic_info = topic_monitor.get_topic_info(topic_name)
-                if topic_info is None:
-                    # The topic is not for being monitored
-                    continue
-
-                is_new_topic = topic_name and topic_name not in topic_monitor.monitored_topics
-                if is_new_topic:
-                    # Register new topic with the monitor
-                    qos_profile = qos_profile_default
-                    if topic_info['reliability'] == 'best_effort':
-                        qos_profile = qos_profile_sensor_data
-                    topic_monitor.add_monitored_topic(topic_name, self.node, qos_profile)
-
-            if topic_monitor.monitored_topics:
-                start_spin_time = time.time()
-                rclpy.spin_once(self.node, 0.05)
+        run_topic_listening(self.node, self.topic_monitor)
 
     def stop(self):
         self.node.destroy_node()
         rclpy.shutdown()
-        return
+
+
+def run_topic_listening(node, topic_monitor):
+    """Subscribe to relevant topics and manage the data received from susbcriptions."""
+    while rclpy.ok():
+        # Check if there is a new topic online
+        # TODO: use graph events
+        topic_names_and_types = node.get_topic_names_and_types()
+        for topic_name in topic_names_and_types.topic_names:
+            # Infer the appropriate QoS profile from the topic name
+            topic_info = topic_monitor.get_topic_info(topic_name)
+            if topic_info is None:
+                # The topic is not for being monitored
+                continue
+
+            is_new_topic = topic_name and topic_name not in topic_monitor.monitored_topics
+            if is_new_topic:
+                # Register new topic with the monitor
+                qos_profile = qos_profile_default
+                if topic_info['reliability'] == 'best_effort':
+                    qos_profile = qos_profile_sensor_data
+                topic_monitor.add_monitored_topic(topic_name, node, qos_profile)
+
+        if topic_monitor.monitored_topics:
+            rclpy.spin_once(node, 0.05)
+
+
+def process_received_data(topic_monitor, show_display=False):
+    """Process data that has been received from topic subscriptions."""
+    if show_display:
+        topic_monitor_display = TopicMonitorDisplay(topic_monitor)
+
+    last_time = time.time()
+    while(1):
+        now = time.time()
+        if now - last_time > time_between_rate_calculations:
+            last_time = now
+            topic_monitor.check_status()
+            topic_monitor.calculate_reception_rates()
+            if show_display:
+                topic_monitor_display.update_display()
 
 
 def main():
-    global WINDOW_SIZE
-
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--display',
-        dest='display_dashboard',
+        dest='show_display',
         action='store_true',
         default=False,
         help='Display the reception rate of topics')
@@ -317,33 +329,40 @@ def main():
         dest='window_size',
         action='store',
         help='Number of messages in reception rate calculation')
-    args = parser.parse_args()
 
+    args = parser.parse_args()
     if args.window_size:
+        global WINDOW_SIZE
         WINDOW_SIZE = args.window_size
 
-    if args.display_dashboard:
-        topic_monitor_dashboard = TopicMonitorDashboard(topic_monitor)
+    if args.show_display:
+        global plt
+        import matplotlib.pyplot as plt
+
+    rclpy.init()
+    topic_monitor = TopicMonitor()
 
     try:
-        thread = RCLPYThread()
-        thread.start()
+        # Run two infinite loops simultaneously: one for receiving data (subscribing to topics and
+        # handling callbacks), and another for processing the received data (calculating the
+        # reception rate and publishing/displaying it).
 
-        last_time = time.time()
+        # Since the display needs to happen in the main thread, we run the "data processing" loop
+        # in the main thread and run the "data receiving" loop in a secondary thread.
 
-        while(1):
-            now = time.time()
-            if now - last_time > time_between_rate_calculations:
-                last_time = now
-                topic_monitor.check_status()
-                topic_monitor.calculate_reception_rates()
-                if args.display_dashboard:
-                    topic_monitor_dashboard.update_dashboard()
-        thread.join()
+        # Start the "data receiving" loop in a new thread
+        data_receiving_thread = DataReceivingThread(topic_monitor)
+        data_receiving_thread.start()
+
+        # Start the "data processing" loop in the main thread
+        process_received_data(topic_monitor, args.show_display)
+
+        # Block this thread until the other thread terminates
+        data_receiving_thread.join()
 
     except KeyboardInterrupt:
-        thread.stop()
-        thread.join()
+        data_receiving_thread.stop()
+        data_receiving_thread.join()
 
 
 if __name__ == '__main__':
