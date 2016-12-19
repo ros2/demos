50e370a5bed580ba68be2e17295ddde5c2e2591d
dhood <dhood@users.noreply.github.com>
2016-12-19 04:10:51 +0100

    Add comments to composition demo

--- composition/src/manual_composition.cpp
+++ composition/src/manual_composition.cpp
@@ -22,9 +22,24 @@
 
 int main(int argc, char * argv[])
 {
+  /**
+   * Initialize any global resources needed by the middleware and the client library.
+   * This will also parse command line arguments one day (as of Beta 1 they are not used).
+   * You must call this before using any other part of the ROS system.
+   * This should be called once per process.
+   */
   rclcpp::init(argc, argv);
+
+  /**
+   * Create an executor that will be responsible for execution of callbacks for a set of nodes.
+   * With this version, all callbacks will be called from within this thread (the main one).
+   */
   rclcpp::executors::SingleThreadedExecutor exec;
 
+  /**
+   * Add some nodes to the executor which provide work for the executor during its "spin" function.
+   * An example of available work is executing a subscription callback, or a timer callback.
+   */
   auto talker = std::make_shared<composition::Talker>();
   exec.add_node(talker);
   auto listener = std::make_shared<composition::Listener>();
@@ -34,6 +49,10 @@ int main(int argc, char * argv[])
   auto client = std::make_shared<composition::Client>();
   exec.add_node(client);
 
+  /**
+   * spin will block until work comes in, execute work as it becomes available, and keep blocking.
+   * It will only be interrupted by Ctrl-C.
+   */
   exec.spin();
   return 0;
 }
