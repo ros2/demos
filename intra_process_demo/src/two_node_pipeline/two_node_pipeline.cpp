20656e100d6fc9b0f293424fc853cdc4424ad056
Matt Hansen <mkhansen-intel@users.noreply.github.com>
2017-02-08 13:48:49 -0800

    Switched producer and consumer node names (#112)

--- intra_process_demo/src/two_node_pipeline/two_node_pipeline.cpp
+++ intra_process_demo/src/two_node_pipeline/two_node_pipeline.cpp
@@ -77,8 +77,8 @@ int main(int argc, char * argv[])
   rclcpp::init(argc, argv);
   rclcpp::executors::SingleThreadedExecutor executor;
 
-  auto producer = std::make_shared<Producer>("consumer", "number");
-  auto consumer = std::make_shared<Consumer>("producer", "number");
+  auto producer = std::make_shared<Producer>("producer", "number");
+  auto consumer = std::make_shared<Consumer>("consumer", "number");
 
   executor.add_node(producer);
   executor.add_node(consumer);
