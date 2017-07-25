8ffbb930051aa66640ace70de75747cffa507974
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2017-07-25 11:08:45 -0700

    make demo_nodes_py a real Python package

--- demo_nodes_py/setup.py
+++ demo_nodes_py/setup.py
@@ -1,5 +1,6 @@
 from ament_python.data_files import get_data_files
 from ament_python.script_dir import install_scripts_to_libexec
+from setuptools import find_packages
 from setuptools import setup
 
 package_name = 'demo_nodes_py'
@@ -9,12 +10,7 @@ install_scripts_to_libexec(package_name)
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[],
-    py_modules=[
-        'topics.listener', 'topics.talker',
-        'topics.listener_qos', 'topics.talker_qos',
-        'services.add_two_ints_client', 'services.add_two_ints_client_async',
-        'services.add_two_ints_server'],
+    packages=find_packages(exclude=['test']),
     data_files=data_files,
     install_requires=['setuptools'],
     author='Esteve Fernandez',
@@ -36,13 +32,13 @@ setup(
     test_suite='test',
     entry_points={
         'console_scripts': [
-            'listener = topics.listener:main',
-            'talker = topics.talker:main',
-            'listener_qos = topics.listener_qos:main',
-            'talker_qos = topics.talker_qos:main',
-            'add_two_ints_client = services.add_two_ints_client:main',
-            'add_two_ints_client_async = services.add_two_ints_client_async:main',
-            'add_two_ints_server = services.add_two_ints_server:main'
+            'listener = demo_nodes_py.topics.listener:main',
+            'talker = demo_nodes_py.topics.talker:main',
+            'listener_qos = demo_nodes_py.topics.listener_qos:main',
+            'talker_qos = demo_nodes_py.topics.talker_qos:main',
+            'add_two_ints_client = demo_nodes_py.services.add_two_ints_client:main',
+            'add_two_ints_client_async = demo_nodes_py.services.add_two_ints_client_async:main',
+            'add_two_ints_server = demo_nodes_py.services.add_two_ints_server:main'
         ],
     },
 )
