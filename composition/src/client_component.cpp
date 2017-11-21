6b556b3aee24f50f64c84623a92c46c884994442
dhood <dhood@users.noreply.github.com>
2017-11-21 14:45:34 -0800

    wait_for_result is not used by client_component

--- composition/src/client_component.cpp
+++ composition/src/client_component.cpp
@@ -26,24 +26,6 @@ using namespace std::chrono_literals;
 namespace composition
 {
 
-template<typename FutureT, typename WaitTimeT>
-std::future_status
-wait_for_result(
-  FutureT & future,
-  WaitTimeT time_to_wait)
-{
-  auto end = std::chrono::steady_clock::now() + time_to_wait;
-  std::chrono::milliseconds wait_period(100);
-  std::future_status status = std::future_status::timeout;
-  do {
-    auto now = std::chrono::steady_clock::now();
-    auto time_left = end - now;
-    if (time_left <= std::chrono::seconds(0)) {break;}
-    status = future.wait_for((time_left < wait_period) ? time_left : wait_period);
-  } while (rclcpp::ok() && status != std::future_status::ready);
-  return status;
-}
-
 Client::Client()
 : Node("Client")
 {
