d4f30ab6ef7ebcb62dd93fa876385bd739eb0db9
dhood <dhood@users.noreply.github.com>
2017-11-21 10:01:13 -0800

    add_two_ints_server inherit from Node

--- demo_nodes_cpp/src/services/add_two_ints_server.cpp
+++ demo_nodes_cpp/src/services/add_two_ints_server.cpp
@@ -12,7 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <iostream>
+#include <cinttypes>
+#include <cstdio>
 #include <memory>
 #include <string>
 
@@ -24,42 +25,61 @@
 void print_usage()
 {
   printf("Usage for add_two_ints_server app:\n");
-  printf("add_two_ints_server [-t topic_name] [-h]\n");
+  printf("add_two_ints_server [-s service_name] [-h]\n");
   printf("options:\n");
   printf("-h : Print this help function.\n");
   printf("-s service_name : Specify the service name for this server. Defaults to add_two_ints.\n");
 }
 
-void handle_add_two_ints(
-  const std::shared_ptr<rmw_request_id_t> request_header,
-  const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> request,
-  std::shared_ptr<example_interfaces::srv::AddTwoInts::Response> response)
-{
-  (void)request_header;
-  std::cout << "Incoming request" << std::endl;
-  std::cout << "a: " << request->a << " b: " << request->b << std::endl;
-  response->sum = request->a + request->b;
-}
 
-int main(int argc, char ** argv)
+class Server : public rclcpp::node::Node
 {
-  rclcpp::init(argc, argv);
+public:
+  explicit Server(const std::string & service_name)
+  : Node("add_two_ints_server")
+  {
+    // Create a callback function for when service requests are received.
+    auto handle_add_two_ints =
+      [](const std::shared_ptr<rmw_request_id_t> request_header,
+        const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> request,
+        std::shared_ptr<example_interfaces::srv::AddTwoInts::Response> response) -> void
+    {
+      (void)request_header;
+      printf("Incoming request\na: %" PRIu64 " b: %" PRIu64 "\n",
+        request->a, request->b);
+      response->sum = request->a + request->b;
+    };
 
-  auto node = rclcpp::Node::make_shared("add_two_ints_server");
+    // Create a service that will use the callback function to handle requests.
+    srv_ = create_service<example_interfaces::srv::AddTwoInts>(service_name, handle_add_two_ints);
+  }
+
+private:
+  rclcpp::service::Service<example_interfaces::srv::AddTwoInts>::SharedPtr srv_;
+};
+
+int main(int argc, char * argv[])
+{
+  // Force flush of the stdout buffer.
+  // This ensures a correct sync of all prints
+  // even when executed simultaneously within the launch file.
+  setvbuf(stdout, NULL, _IONBF, BUFSIZ);
 
   if (rcutils_cli_option_exist(argv, argv + argc, "-h")) {
     print_usage();
     return 0;
   }
 
-  auto topic = std::string("add_two_ints");
-  if (rcutils_cli_option_exist(argv, argv + argc, "-s")) {
-    topic = std::string(rcutils_cli_get_option(argv, argv + argc, "-s"));
-  }
-  auto server =
-    node->create_service<example_interfaces::srv::AddTwoInts>(topic, handle_add_two_ints);
+  rclcpp::init(argc, argv);
 
+  auto service_name = std::string("add_two_ints");
+  if (rcutils_cli_option_exist(argv, argv + argc, "-s")) {
+    service_name = std::string(rcutils_cli_get_option(argv, argv + argc, "-s"));
+  }
+
+  auto node = std::make_shared<Server>(service_name);
   rclcpp::spin(node);
 
+  rclcpp::shutdown();
   return 0;
 }
