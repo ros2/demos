c0c8770945c892008740c33ebc1125e715679b1a
dhood <dhood@users.noreply.github.com>
2017-11-27 15:06:44 -0800

    Native talker inherit from Node

--- demo_nodes_cpp_native/src/talker.cpp
+++ demo_nodes_cpp_native/src/talker.cpp
@@ -23,42 +23,51 @@
 #include "rmw_fastrtps_cpp/get_participant.hpp"
 #include "rmw_fastrtps_cpp/get_publisher.hpp"
 
+using namespace std::chrono_literals;
+
+class Talker : public rclcpp::Node
+{
+public:
+  Talker()
+  : Node("talker_native")
+  {
+    {
+      rcl_node_t * rcl_node = get_node_base_interface()->get_rcl_node_handle();
+      rmw_node_t * rmw_node = rcl_node_get_rmw_handle(rcl_node);
+      eprosima::fastrtps::Participant * p = rmw_fastrtps_cpp::get_participant(rmw_node);
+      printf("eprosima::fastrtps::Participant * %zu\n", reinterpret_cast<size_t>(p));
+    }
+
+    msg_ = std::make_shared<std_msgs::msg::String>();
+    auto publish = [this]() -> void
+    {
+      msg_->data = "Hello World: " + std::to_string(count_++);
+      printf("Publishing: '%s'\n", msg_->data.c_str());
+      pub_->publish(msg_);
+    };
+    timer_ = create_wall_timer(500ms, publish);
+    pub_ = create_publisher<std_msgs::msg::String>("chatter");
+
+    {
+      rcl_publisher_t * rcl_pub = pub_->get_publisher_handle();
+      rmw_publisher_t * rmw_pub = rcl_publisher_get_rmw_handle(rcl_pub);
+      eprosima::fastrtps::Publisher * p = rmw_fastrtps_cpp::get_publisher(rmw_pub);
+      printf("eprosima::fastrtps::Publisher * %zu\n", reinterpret_cast<size_t>(p));
+    }
+  }
+
+private:
+  size_t count_ = 1;
+  std::shared_ptr<std_msgs::msg::String> msg_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
+  rclcpp::TimerBase::SharedPtr timer_;
+};
+
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-
-  auto node = rclcpp::node::Node::make_shared("talker_native");
-
-  {
-    rcl_node_t * rcl_node = node->get_node_base_interface()->get_rcl_node_handle();
-    rmw_node_t * rmw_node = rcl_node_get_rmw_handle(rcl_node);
-    eprosima::fastrtps::Participant * p = rmw_fastrtps_cpp::get_participant(rmw_node);
-    printf("eprosima::fastrtps::Participant * %zu\n", reinterpret_cast<size_t>(p));
-  }
-
-  auto pub = node->create_publisher<std_msgs::msg::String>("chatter");
-
-  {
-    rcl_publisher_t * rcl_pub = pub->get_publisher_handle();
-    rmw_publisher_t * rmw_pub = rcl_publisher_get_rmw_handle(rcl_pub);
-    eprosima::fastrtps::Publisher * p = rmw_fastrtps_cpp::get_publisher(rmw_pub);
-    printf("eprosima::fastrtps::Publisher * %zu\n", reinterpret_cast<size_t>(p));
-  }
-
-  rclcpp::WallRate loop_rate(2);
-
-  auto msg = std::make_shared<std_msgs::msg::String>();
-  auto i = 1;
-
-  while (rclcpp::ok()) {
-    msg->data = "Hello World: " + std::to_string(i++);
-    std::cout << "Publishing: '" << msg->data << "'" << std::endl;
-    pub->publish(msg);
-    rclcpp::spin_some(node);
-    loop_rate.sleep();
-  }
-
+  auto node = std::make_shared<Talker>();
+  rclcpp::spin(node);
   rclcpp::shutdown();
-
   return 0;
 }
