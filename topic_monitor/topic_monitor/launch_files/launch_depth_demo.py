cc7c6df82ec6917968cbdd5ff5ba1f9ca453316b
dhood <dhood@users.noreply.github.com>
2017-12-06 13:30:43 -0800

    move lower so not repeated

--- topic_monitor/topic_monitor/launch_files/launch_depth_demo.py
+++ topic_monitor/topic_monitor/launch_files/launch_depth_demo.py
@@ -26,8 +26,6 @@ def add_process_to_descriptor(launch_descriptor, size, depth):
     package = 'topic_monitor'
     executable = get_executable_path(package_name=package, executable_name='data_publisher')
 
-    os.environ['PYTHONUNBUFFERED'] = '1'  # force unbuffered output to get prints to sync correctly
-
     launch_descriptor.add_process(
         cmd=[executable, name, '--depth', str(depth), '--payload-size', str(payload)],
         name=name,
@@ -37,6 +35,9 @@ def add_process_to_descriptor(launch_descriptor, size, depth):
 def main():
     launcher = DefaultLauncher()
     launch_descriptor = LaunchDescriptor()
+
+    os.environ['PYTHONUNBUFFERED'] = '1'  # force unbuffered output to get prints to sync correctly
+
     add_process_to_descriptor(launch_descriptor, 'small', 1)
     add_process_to_descriptor(launch_descriptor, 'small', 50)
     add_process_to_descriptor(launch_descriptor, 'large', 1)
