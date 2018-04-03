7284573f46ae15d9bdb0f09b525c77a1ac94849a
dhood <dhood@users.noreply.github.com>
2018-04-04 09:03:05 +1000

    Pendulum teleop flakiness reduction (#232)
    * Sleep before exiting to get msgs out
    
    * Avoid interrupting already exiting pendulum demo
    
    * Use printf
    
    * Subscribe to teleop msg with transient local
    
    Makes test more robust to startup order
    
    * Force flush for output sync

--- pendulum_control/src/pendulum_teleop.cpp
+++ pendulum_control/src/pendulum_teleop.cpp
@@ -54,7 +54,9 @@ int main(int argc, char * argv[])
   rclcpp::sleep_for(500ms);
   pub->publish(msg);
   rclcpp::spin_some(teleop_node);
-  std::cout << "Teleop node exited." << std::endl;
+  printf("Teleop message published.\n");
+  rclcpp::sleep_for(1s);
+  printf("Teleop node exited.\n");
 
   rclcpp::shutdown();
 }
