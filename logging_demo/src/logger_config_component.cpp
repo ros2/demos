57ac2f4ce211c99cc76aae020db5dc20923f4b35
dhood <dhood@users.noreply.github.com>
2018-06-07 14:08:14 -0700

    Use log level parsing from rcutils
    Also allows for case insensitivity of the severity string

--- logging_demo/src/logger_config_component.cpp
+++ logging_demo/src/logger_config_component.cpp
@@ -21,6 +21,7 @@
 #include "logging_demo/srv/config_logger.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rcutils/error_handling.h"
+#include "rcutils/logging.h"
 
 namespace logging_demo
 {
@@ -44,28 +45,27 @@ LoggerConfig::handle_logger_config_request(
     this->get_logger(), "Incoming request: logger '%s', severity '%s'",
     request->logger_name.c_str(), severity_string);
   std::flush(std::cout);
+
   int severity;
-  if (strcmp("DEBUG", severity_string) == 0) {
-    severity = RCUTILS_LOG_SEVERITY_DEBUG;
-  } else if (strcmp("INFO", severity_string) == 0) {
-    severity = RCUTILS_LOG_SEVERITY_INFO;
-  } else if (strcmp("WARN", severity_string) == 0) {
-    severity = RCUTILS_LOG_SEVERITY_WARN;
-  } else if (strcmp("ERROR", severity_string) == 0) {
-    severity = RCUTILS_LOG_SEVERITY_ERROR;
-  } else if (strcmp("FATAL", severity_string) == 0) {
-    severity = RCUTILS_LOG_SEVERITY_FATAL;
-  } else if (strcmp("UNSET", severity_string) == 0) {
-    severity = RCUTILS_LOG_SEVERITY_UNSET;
-  } else {
+  rcutils_ret_t ret = rcutils_logging_severity_level_from_string(
+    severity_string, rcl_get_default_allocator(), &severity);
+  if (RCUTILS_RET_LOGGING_SEVERITY_STRING_INVALID == ret) {
     RCLCPP_ERROR(
       this->get_logger(), "Unknown severity '%s'", severity_string);
     response->success = false;
     return;
   }
+  if (RCUTILS_RET_OK != ret) {
+    RCLCPP_ERROR(
+      this->get_logger(), "Error %d getting severity level from request: %s", ret,
+      rcl_get_error_string());
+    rcl_reset_error();
+    response->success = false;
+    return;
+  }
 
   // TODO(dhood): allow configuration through rclcpp
-  auto ret = rcutils_logging_set_logger_level(request->logger_name.c_str(), severity);
+  ret = rcutils_logging_set_logger_level(request->logger_name.c_str(), severity);
   if (ret != RCUTILS_RET_OK) {
     RCLCPP_ERROR(
       this->get_logger(), "Error setting severity: %s", rcutils_get_error_string_safe());
