3b42efb2a85a4b4fee9465bf9b7d2ab875dd3cc3
William Woodall <william+github@osrfoundation.org>
2018-06-15 16:35:50 -0700

    add talker_listener.launch.py new-style launch file (#244)
    * add talker_listener.launch.py new-style launch file
    
    * add more launch demos
    
    * add exec dependency on launch_ros

---
+++ demo_nodes_cpp/launch/topics/talker_listener.launch.py
@@ -0,0 +1,27 @@
+# Copyright 2018 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Launch a talker and a listener."""
+
+from launch import LaunchDescription
+import launch_ros.actions
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        launch_ros.actions.Node(
+            package='demo_nodes_cpp', node_executable='talker', output='screen'),
+        launch_ros.actions.Node(
+            package='demo_nodes_cpp', node_executable='listener', output='screen'),
+    ])
