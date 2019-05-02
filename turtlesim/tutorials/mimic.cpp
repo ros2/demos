f6125bf765008b1961d6326a0133717fcdb6c7a2
Darby Lim <thlim@robotis.com>
2019-05-02 17:57:11 +0900

    Port tutorials to ROS2
    Signed-off-by: Darby Lim <thlim@robotis.com>

--- turtlesim/tutorials/mimic.cpp
+++ turtlesim/tutorials/mimic.cpp
@@ -1,40 +1,59 @@
-#include <ros/ros.h>
-#include <turtlesim/Pose.h>
-#include <geometry_msgs/Twist.h>
+#include <rclcpp/rclcpp.hpp>
+#include <turtlesim/msg/Pose.hpp>
+#include <geometry_msgs/msg/Twist.hpp>
 
-class Mimic
+class Mimic : public rclcpp::Node
+  : Node("turtle_mimic")
 {
-public:
-  Mimic();
+ public:
+  explicit Mimic();
 
-private:
-  void poseCallback(const turtlesim::PoseConstPtr& pose);
+ private:
+  // void poseCallback(const turtlesim::PoseConstPtr& pose);
 
-  ros::Publisher twist_pub_;
-  ros::Subscriber pose_sub_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_pub_;
+  rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr pose_sub_;
 };
 
 Mimic::Mimic()
 {
-  ros::NodeHandle input_nh("input");
-  ros::NodeHandle output_nh("output");
-  twist_pub_ = output_nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
-  pose_sub_ = input_nh.subscribe<turtlesim::Pose>("pose", 1, &Mimic::poseCallback, this);
+  // ros::NodeHandle input_nh("input");
+  // ros::NodeHandle output_nh("output");
+
+  twist_pub_ = create_publisher<geometry_msgs::msg::Twist>("output/cmd_vel", rmw_qos_profile_default);
+
+  auto pose_call_back = 
+    [this](const turtlesim::msg::Pose::SharedPtr pose) -> void
+    {
+      auto twist = std::make_shared<geometry_msgs::msg::Twist>();
+      twist->angular.z = pose->angular_velocity;
+      twist->linear.x = pose->linear_velocity;
+      twist_pub_->publish(twist);
+    };
+
+  pose_sub_ = create_subscription<turtlesim::msg::Pose>("input/pose", pose_call_back);
 }
 
-void Mimic::poseCallback(const turtlesim::PoseConstPtr& pose)
-{
-  geometry_msgs::Twist twist;
-  twist.angular.z = pose->angular_velocity;
-  twist.linear.x = pose->linear_velocity;
-  twist_pub_.publish(twist);
-}
+// void Mimic::poseCallback(const turtlesim::PoseConstPtr& pose)
+// {
+  // geometry_msgs::Twist twist;
+  // twist.angular.z = pose->angular_velocity;
+  // twist.linear.x = pose->linear_velocity;
+  // twist_pub_.publish(twist);
+// }
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "turtle_mimic");
-  Mimic mimic;
+  setvbuf(stdout, NULL, _IONBF, BUFSIZ);
 
-  ros::spin();
+  rclcpp::init(argc, argv);
+  
+  auto node = std::make_shared<Mimic>();
+
+  rclcpp::spin(node);
+
+  rclcpp::shutdown();
+
+  return 0;
 }
 
