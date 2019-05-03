eb4f9ba2a6dd36644b2eaa2abf59af5855435189
Darby Lim <thlim@robotis.com>
2019-05-03 15:17:45 +0900

    add empty client and printPresent()
    Signed-off-by: Darby Lim <thlim@robotis.com>

--- turtlesim/tutorials/draw_square.cpp
+++ turtlesim/tutorials/draw_square.cpp
@@ -29,15 +29,19 @@ class DrawSquare : public rclcpp::Node
     g_pose_ = std::make_shared<turtlesim::msg::Pose>();
     g_goal_ = std::make_shared<turtlesim::msg::Pose>();
 
+    reset_client_ = create_client<std_srvs::srv::Empty>("reset");
+    reset_request();
+
     auto pose_callback = 
       [this](const turtlesim::msg::Pose::SharedPtr pose) -> void
       {
         g_pose_ = pose;
+        printPresent();
       };
 
-    pose_sub_ = create_subscription<turtlesim::msg::Pose>("pose", pose_callback);
+    pose_sub_ = create_subscription<turtlesim::msg::Pose>("turtle1/pose", pose_callback);
 
-    twist_pub_ = create_publisher<geometry_msgs::msg::Twist>("cmd_vel", rmw_qos_profile_default);
+    twist_pub_ = create_publisher<geometry_msgs::msg::Twist>("turtle1/cmd_vel", rmw_qos_profile_default);
 
     auto timer_callback = 
       [this]() -> void
@@ -79,11 +83,6 @@ class DrawSquare : public rclcpp::Node
   }
 
  private:
-  // void poseCallback(const turtlesim::PoseConstPtr& pose)
-  // {
-    // g_pose_ = pose;
-  // }
-
   bool hasReachedGoal()
   {
     return fabsf(g_pose_->x - g_goal_->x) < 0.1 && fabsf(g_pose_->y - g_goal_->y) < 0.1 && fabsf(g_pose_->theta - g_goal_->theta) < 0.01;
@@ -97,6 +96,11 @@ class DrawSquare : public rclcpp::Node
   void printGoal()
   {
     RCLCPP_INFO(this->get_logger(), "New goal [%f %f, %f]", g_goal_->x, g_goal_->y, g_goal_->theta);
+  }
+
+  void printPresent()
+  {
+    RCLCPP_INFO(this->get_logger(), "Present [%f %f, %f]", g_pose_->x, g_pose_->y, g_pose_->theta);
   }
 
   void commandTurtle(float linear, float angular)
@@ -170,41 +174,29 @@ class DrawSquare : public rclcpp::Node
     }
   }
 
-  // void timerCallback(const ros::TimerEvent&, ros::Publisher twist_pub)
-  // {
-    // if (!g_pose_)
-    // {
-      // return;
-    // }
+  void reset_request()
+  {
+    while (!reset_client_->wait_for_service(1s)) 
+    {
+      if (!rclcpp::ok()) 
+      {
+        RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for the service. Exiting.");
+        return;
+      }
+      RCLCPP_INFO(this->get_logger(), "Service not available, waiting again...");
+    }
+    auto request = std::make_shared<std_srvs::srv::Empty::Request>();
 
-    // if (!g_first_goal_set_)
-    // {
-      // g_first_goal_set_ = true;
-      // g_state_ = FORWARD;
-      // g_goal_->x = cos(g_pose_->theta) * 2 + g_pose_->x;
-      // g_goal_->y = sin(g_pose_->theta) * 2 + g_pose_->y;
-      // g_goal_->theta = g_pose_->theta;
-      // printGoal();
-    // }
-
-    // if (g_state_ == FORWARD)
-    // {
-      // forward(twist_pub);
-    // }
-    // else if (g_state_ == STOP_FORWARD)
-    // {
-      // stopForward(twist_pub);
-    // }
-    // else if (g_state_ == TURN)
-    // {
-      // turn(twist_pub);
-    // }
-    // else if (g_state_ == STOP_TURN)
-    // {
-      // stopTurn(twist_pub);
-    // }
-  // }
+    using ServiceResponseFuture = rclcpp::Client<std_srvs::srv::Empty>::SharedFuture;
+    auto response_received_callback = 
+      [this](ServiceResponseFuture future)
+      {
+        auto result = future.get();
+      };
+    auto future_result = reset_client_->async_send_request(request, response_received_callback);
+  }
  
+ private:
   std::shared_ptr<turtlesim::msg::Pose> g_pose_;
   std::shared_ptr<turtlesim::msg::Pose> g_goal_;
 
@@ -213,19 +205,17 @@ class DrawSquare : public rclcpp::Node
 
   bool g_first_goal_set_;
 
-  rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr pose_sub_; // = nh.subscribe("turtle1/pose", 1, poseCallback);
+  rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr pose_sub_; 
 
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_pub_;
 
-  // rclcpp::ServiceClient<std_srvs::srv::Empty> reset_; // = nh.serviceClient<std_srvs::srv::Empty>("reset");
+  rclcpp::Client<std_srvs::srv::Empty>::SharedPtr reset_client_;
 
-  rclcpp::TimerBase::SharedPtr timer_; // = nh.createTimer(ros::Duration(0.016), boost::bind(timerCallback, _1, twist_pub));
+  rclcpp::TimerBase::SharedPtr timer_;
 };
 
 int main(int argc, char** argv)
 {
-  setvbuf(stdout, NULL, _IONBF, BUFSIZ);
-
   rclcpp::init(argc, argv);
 
   auto node = std::make_shared<DrawSquare>();
@@ -235,16 +225,4 @@ int main(int argc, char** argv)
   rclcpp::shutdown();
 
   return 0;
-
-  // ros::NodeHandle nh;
-
-  // ros::Subscriber pose_sub = nh.subscribe("turtle1/pose", 1, poseCallback);
-  // ros::Publisher twist_pub = nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1);
-  // ros::ServiceClient reset = nh.serviceClient<std_srvs::Empty>("reset");
-  // ros::Timer timer = nh.createTimer(ros::Duration(0.016), boost::bind(timerCallback, _1, twist_pub));
-
-  // std_srvs::Empty empty;
-  // reset.call(empty);
-
-  // ros::spin();
 }
