d7161d78bc6bda7cf3b22fa95b3b8e5320e6b9f2
William Woodall <william@osrfoundation.org>
2019-05-10 10:53:49 -0700

    add parameter_blackboard to make some tutorials easier (#333)
    * add parameter_blackboard to make some tutorials easier
    
    Signed-off-by: William Woodall <william@osrfoundation.org>
    
    * remove unused include
    
    Signed-off-by: William Woodall <william@osrfoundation.org>

---
+++ demo_nodes_cpp/src/parameters/parameter_blackboard.cpp
@@ -0,0 +1,46 @@
+// Copyright 2019 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <memory>
+#include <string>
+
+#include "rcl_interfaces/srv/list_parameters.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+class ParameterBlackboard : public rclcpp::Node
+{
+public:
+  ParameterBlackboard(
+    const std::string & name = "parameter_blackboard",
+    const std::string & namespace_ = "",
+    const rclcpp::NodeOptions & options = (
+      rclcpp::NodeOptions()
+      .allow_undeclared_parameters(true)
+      .automatically_declare_initial_parameters(true)
+  ))
+  : rclcpp::Node(name, namespace_, options)
+  {
+    RCLCPP_INFO(this->get_logger(),
+      "Parameter blackboard node named '%s' ready, and serving '%zu' parameters already!",
+      this->get_fully_qualified_name(), this->list_parameters(
+        {}, rcl_interfaces::srv::ListParameters::Request::DEPTH_RECURSIVE).names.size());
+  }
+};
+
+int main(int argc, char const * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<ParameterBlackboard>());
+  return 0;
+}
