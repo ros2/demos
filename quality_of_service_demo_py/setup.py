6e2d51f694079d5800eb89871adc6e372197970a
Emerson Knapp <eknapp@amazon.com>
2019-05-16 15:17:54 -0700

    Initial commit for quality_of_service_demo_py - working deadline demo
    Signed-off-by: Emerson Knapp <eknapp@amazon.com>

---
+++ quality_of_service_demo_py/setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages
+from setuptools import setup
+
+package_name = 'quality_of_service_demo_py'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Emerson Knapp',
+    maintainer='Amazon ROS Contributions',
+    maintainer_email='ros-contributions@amazon.com',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description=(
+        'Python nodes to demonstrate ROS2 QoS policies.'
+    ),
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'lifespan = quality_of_service_demo_py.lifespan:main',
+        ],
+    },
+)
