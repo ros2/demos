c7297482e618ec709b22ee77d8f36aa582aaa2be
Karsten Knese <karsten@openrobotics.org>
2019-09-28 19:34:13 -0700

    loaned message talker demo
    Signed-off-by: Karsten Knese <karsten@openrobotics.org>

---
+++ demo_nodes_cpp/src/topics/talker_loaned_message.cpp
@@ -0,0 +1,107 @@
+// Copyright 2019 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <chrono>
+#include <cstdio>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_components/register_node_macro.hpp"
+#include "rcutils/cmdline_parser.h"
+
+#include "std_msgs/msg/string.hpp"
+
+#include "demo_nodes_cpp/visibility_control.h"
+
+using namespace std::chrono_literals;
+
+namespace demo_nodes_cpp
+{
+// Create a Talker class that subclasses the generic rclcpp::Node base class.
+// The main function below will instantiate the class as a ROS node.
+class LoanedMessageTalker : public rclcpp::Node
+{
+public:
+  DEMO_NODES_CPP_PUBLIC
+  explicit LoanedMessageTalker(const rclcpp::NodeOptions & options)
+  : Node("loaned_message_talker", options)
+  {
+    // Create a function for when messages are to be sent.
+    setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+    std::vector<std::string> args = options.arguments();
+    if (find_command_option(args, "-h")) {
+      print_usage();
+      rclcpp::shutdown();
+    } else {
+      std::string tmptopic = get_command_option(args, "-t");
+      if (!tmptopic.empty()) {
+        topic_name_ = tmptopic;
+      }
+      auto publish_message =
+        [this]() -> void
+        {
+          auto loaned_msg = pub_->loan_message();
+          auto msg_data = "Hello World: " + std::to_string(count_++);
+          loaned_msg.get().data = msg_data;
+          RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", msg_data.c_str());
+          pub_->publish(std::move(loaned_msg));
+        };
+      // Create a publisher with a custom Quality of Service profile.
+      rclcpp::QoS qos(rclcpp::KeepLast(7));
+      pub_ = this->create_publisher<std_msgs::msg::String>(topic_name_, qos);
+
+      // Use a timer to schedule periodic message publishing.
+      timer_ = this->create_wall_timer(1s, publish_message);
+    }
+  }
+
+private:
+  DEMO_NODES_CPP_LOCAL
+  void print_usage()
+  {
+    printf("Usage for talker app:\n");
+    printf("talker [-t topic_name] [-h]\n");
+    printf("options:\n");
+    printf("-h : Print this help function.\n");
+    printf("-t topic_name : Specify the topic on which to publish. Defaults to chatter.\n");
+  }
+
+  DEMO_NODES_CPP_LOCAL
+  bool find_command_option(const std::vector<std::string> & args, const std::string & option)
+  {
+    return std::find(args.begin(), args.end(), option) != args.end();
+  }
+
+  DEMO_NODES_CPP_LOCAL
+  std::string get_command_option(const std::vector<std::string> & args, const std::string & option)
+  {
+    auto it = std::find(args.begin(), args.end(), option);
+    if (it != args.end() && ++it != args.end()) {
+      return *it;
+    }
+    return std::string();
+  }
+
+  size_t count_ = 1;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  std::string topic_name_ = "chatter";
+};
+
+}  // namespace demo_nodes_cpp
+
+RCLCPP_COMPONENTS_REGISTER_NODE(demo_nodes_cpp::LoanedMessageTalker)
