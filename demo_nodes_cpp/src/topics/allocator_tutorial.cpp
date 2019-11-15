2793f36345f94fb8e86dc5bf2ab9c6574fa7c14c
Chris Lalancette <clalancette@openrobotics.org>
2019-11-15 10:59:37 -0500

    Add in a more helpful usage message to allocator_tutorial. (#409)
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- demo_nodes_cpp/src/topics/allocator_tutorial.cpp
+++ demo_nodes_cpp/src/topics/allocator_tutorial.cpp
@@ -143,6 +143,14 @@ int main(int argc, char ** argv)
   std::list<std::string> keys = {"intra", "intraprocess", "intra-process", "intra_process"};
   bool intra_process = false;
 
+  printf("This simple demo shows off a custom memory allocator to count all\n"
+    "instances of new/delete in the program.  It can be run in either regular\n"
+    "mode (no arguments), or in intra-process mode (by passing 'intra' as a\n"
+    "command-line argument)'.  It will then publish a message to the\n"
+    "'/allocator_tutorial' topic every 10 milliseconds until Ctrl-C is pressed.\n"
+    "At that time it will print a count of the number of allocations and\n"
+    "deallocations that happened during the program.\n\n");
+
   if (argc > 1) {
     for (auto & key : keys) {
       if (std::string(argv[1]) == key) {
