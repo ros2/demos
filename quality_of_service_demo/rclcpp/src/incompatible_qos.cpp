d0c5b231caecbeedbe96cfac3c608b7968af7d3d
Jaison Titus <jaisontj92@gmail.com>
2019-11-21 16:31:18 -0800

    Improved rclcpp demo for incompatible qos.
    Signed-off-by: Jaison Titus <jaisontj92@gmail.com>

--- quality_of_service_demo/rclcpp/src/incompatible_qos.cpp
+++ quality_of_service_demo/rclcpp/src/incompatible_qos.cpp
@@ -26,23 +26,87 @@
 
 #include "quality_of_service_demo/common_nodes.hpp"
 
+void print_usage()
+{
+  printf("Usage:\n");
+  printf("incompatible_qos "
+    "incompatible_qos_policy_name "
+    "[-h]\n");
+  printf("required arguments:\n");
+  printf("incompatible_qos_policy_name: "
+    "The QoS Policy which will have an incompatible value.\n"
+	" durability, deadline, liveliness_policy, liveliness_lease_duration and reliability\n");
+  printf("optional arguments:\n");
+  printf("-h : Print this help message.\n");
+}
+
 int main(int argc, char * argv[])
 {
   // Force flush of the stdout buffer.
   setvbuf(stdout, NULL, _IONBF, BUFSIZ);
-  std::string topic("qos_deadline_chatter");
+
+  // Argument count and usage
+  if (argc < 2 || rcutils_cli_option_exist(argv, argv + argc, "-h")) {
+    print_usage();
+    return 0;
+  }
+
+  rclcpp::QoS qos_profile_publisher(10);
+  rclcpp::QoS qos_profile_subscription(10);
+
+  // Configuration variables
+  std::string qos_policy_name = argv[1];
+  if (qos_policy_name.compare("durability") == 0) {
+	  printf("Durability incompatibility selected.\n"
+			  "Incompatibility condition: publisher durability kind < "
+			  "subscripition durability kind.\n"
+			  "Setting publisher durability to: VOLATILE\n"
+			  "Setting subscription durability to: TRANSIENT_LOCAL\n");
+	  qos_profile_publisher.durability(RMW_QOS_POLICY_DURABILITY_VOLATILE);
+	  qos_profile_subscription.durability(RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL);
+  } else if (qos_policy_name.compare("deadline") == 0) {
+	  printf("Deadline incompatibility selected.\n"
+			  "Incompatibility condition: publisher deadline > subscription deadline\n"
+			  "Setting publisher deadline to: 2 seconds\n"
+			  "Setting subscription deadline to: 1 second\n");
+	  qos_profile_publisher.deadline(std::chrono::milliseconds(2000));
+	  qos_profile_subscription.deadline(std::chrono::milliseconds(1000));
+  } else if (qos_policy_name.compare("liveliness_policy") == 0) {
+	  printf("Liveliness Policy incompatibility selected.\n"
+			  "Incompatibility condition: publisher liveliness policy < "
+			  "subscripition liveliness policy.\n"
+			  "Setting publisher liveliness policy to: MANUAL_BY_NODE\n"
+			  "Setting subscription liveliness policy to: MANUAL_BY_TOPIC\n");
+	  qos_profile_publisher.liveliness(RMW_QOS_POLICY_LIVELINESS_MANUAL_BY_NODE);
+	  qos_profile_subscription.liveliness(RMW_QOS_POLICY_LIVELINESS_MANUAL_BY_TOPIC);
+  } else if (qos_policy_name.compare("liveliness_lease_duration") == 0) {
+	  printf("Liveliness lease duration incompatibility selected.\n"
+			  "Incompatibility condition: publisher liveliness lease duration > "
+			  "subscription liveliness lease duration\n"
+			  "Setting publisher liveliness lease duration to: 2 seconds\n"
+			  "Setting subscription liveliness lease duration to: 1 seconds\n");
+	  qos_profile_publisher.liveliness_lease_duration(std::chrono::milliseconds(2000));
+	  qos_profile_subscription.liveliness_lease_duration(std::chrono::milliseconds(1000));
+  } else if (qos_policy_name.compare("reliability") == 0) {
+	  printf("Reliability incompatibility selected.\n"
+			  "Incompatibility condition: publisher reliability < subscripition reliability.\n"
+			  "Setting publisher reliability to: BEST_EFFORT\n"
+			  "Setting subscription reliability to: RELIABLE\n");
+	  qos_profile_publisher.reliability(RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT);
+	  qos_profile_subscription.reliability(RMW_QOS_POLICY_RELIABILITY_RELIABLE);
+  } else {
+	  printf("%s is not a valid qos policy name\n", qos_policy_name.c_str());
+	  print_usage();
+	  return 0;
+  }
+  printf("\n\n");
+  std::string topic("qos_incompatible_qos_chatter");
 
   // Initialization and configuration
   rclcpp::init(argc, argv);
   rclcpp::executors::SingleThreadedExecutor executor;
 
-  rclcpp::QoS qos_profile_publisher(10);
-  qos_profile_publisher.deadline(std::chrono::milliseconds(2000));
-
-  rclcpp::QoS qos_profile_subscription(10);
-  qos_profile_subscription.deadline(std::chrono::milliseconds(1000));
-
-  auto talker = std::make_shared<Talker>(qos_profile_publisher, topic);
+  auto talker = std::make_shared<Talker>(qos_profile_publisher, topic, 5);
   talker->get_options().event_callbacks.incompatible_qos_callback =
     [node = talker.get()](rclcpp::QOSOfferedIncompatibleQoSInfo & event) -> void
     {
@@ -63,12 +127,6 @@ int main(int argc, char * argv[])
 
   talker->initialize();
   listener->initialize();
-
-  auto pause_timer = talker->create_wall_timer(
-    std::chrono::milliseconds(1000),
-    [talker]() -> void {
-      talker->pause_publish_for(std::chrono::milliseconds(5000));
-    });
 
   // Execution
   executor.add_node(talker);
