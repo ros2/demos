a66f0e894841a5d751bce6ded4983acb780448cf
y-okumura-isp <60122040+y-okumura-isp@users.noreply.github.com>
2021-01-20 00:04:52 +0900

    Fix leak(#480) (#481)
    * Fix leak(#480)
    
    Signed-off-by: y-okumura-isp <y-okumura@isp.co.jp>

--- composition/src/linktime_composition.cpp
+++ composition/src/linktime_composition.cpp
@@ -15,6 +15,7 @@
 #include <memory>
 #include <string>
 #include <vector>
+#include <utility>
 
 #include "class_loader/class_loader.hpp"
 #include "rclcpp/rclcpp.hpp"
@@ -32,7 +33,7 @@ int main(int argc, char * argv[])
   rclcpp::Logger logger = rclcpp::get_logger(LINKTIME_COMPOSITION_LOGGER_NAME);
   rclcpp::executors::SingleThreadedExecutor exec;
   rclcpp::NodeOptions options;
-  std::vector<class_loader::ClassLoader *> loaders;
+  std::vector<std::unique_ptr<class_loader::ClassLoader>> loaders;
   std::vector<rclcpp_components::NodeInstanceWrapper> node_wrappers;
 
   std::vector<std::string> libraries = {
@@ -42,7 +43,7 @@ int main(int argc, char * argv[])
   };
   for (auto library : libraries) {
     RCLCPP_INFO(logger, "Load library %s", library.c_str());
-    auto loader = new class_loader::ClassLoader(library);
+    auto loader = std::make_unique<class_loader::ClassLoader>(library);
     auto classes = loader->getAvailableClasses<rclcpp_components::NodeFactory>();
     for (auto clazz : classes) {
       RCLCPP_INFO(logger, "Instantiate class %s", clazz.c_str());
@@ -52,7 +53,7 @@ int main(int argc, char * argv[])
       node_wrappers.push_back(wrapper);
       exec.add_node(node);
     }
-    loaders.push_back(loader);
+    loaders.push_back(std::move(loader));
   }
 
   exec.spin();
