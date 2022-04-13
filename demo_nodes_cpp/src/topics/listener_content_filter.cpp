ce4131709b5fcb23610863ba85f6fcd2e1e58a7b
Chen Lihui <lihui.chen@sony.com>
2022-04-13 12:50:56 +0800

    use = instead of like
    Signed-off-by: Chen Lihui <lihui.chen@sony.com>

--- demo_nodes_cpp/src/topics/listener_content_filter.cpp
+++ demo_nodes_cpp/src/topics/listener_content_filter.cpp
@@ -30,25 +30,23 @@ public:
   explicit ListenerContentFilter(const rclcpp::NodeOptions & options)
   : Node("content_filter_listener", options)
   {
-    // Create a callback function for when messages are received.
-    // Variations of this function also exist using, for example UniquePtr for zero-copy transport.
     setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+    // Create a callback function for when messages are received.
     auto callback =
-      [this](std_msgs::msg::String::ConstSharedPtr msg) -> void
+      [this](const std_msgs::msg::String & msg) -> void
       {
-        RCLCPP_INFO(this->get_logger(), "I heard: [%s]", msg->data.c_str());
+        RCLCPP_INFO(this->get_logger(), "I heard: [%s]", msg.data.c_str());
       };
     // Create a subscription to the topic which can be matched with one or more compatible ROS
     // publishers.
     // Note that not all publishers on the same topic with the same type will be compatible:
     // they must have compatible Quality of Service policies.
 
-    // Initialize a subscription with a content filter to receive messages that contain a prefix
-    // with 'Hello World: 1', which is similar to the SQL statement `data like 'Hello World: 1%'`.
-    // NOTE: rmw_connextdds does not support `like` operation currently.
+    // Initialize a subscription with a content filter to receive messages that are
+    // 'Hello World: 10'.
     rclcpp::SubscriptionOptions sub_options;
-    sub_options.content_filter_options.filter_expression = "data like %0";
-    sub_options.content_filter_options.expression_parameters = {"'Hello World: 1%'"};
+    sub_options.content_filter_options.filter_expression = "data = %0";
+    sub_options.content_filter_options.expression_parameters = {"'Hello World: 10'"};
 
     sub_ = create_subscription<std_msgs::msg::String>("chatter", 10, callback, sub_options);
 
