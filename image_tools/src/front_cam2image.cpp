2f39f016c1cb6a6331476a4e648a10714eb02367
johnnyliu2886 <johnnyliu2887@gmail.com>
2022-11-22 18:15:56 +0800

    change the name from cam2image_custom_0,2 to front_cam2image and back_cam2image, reduce fps from 30 fps to 15 fps

--- image_tools/src/front_cam2image.cpp
+++ image_tools/src/front_cam2image.cpp
@@ -79,13 +79,13 @@ int main(int argc, char * argv[])
   // Initialize default demo parameters
   bool show_camera = false;
   size_t depth = rmw_qos_profile_default.depth;
-  double freq = 30.0;
+  double freq = 15.0;
   rmw_qos_reliability_policy_t reliability_policy = rmw_qos_profile_default.reliability;
   rmw_qos_history_policy_t history_policy = rmw_qos_profile_default.history;
-  size_t width = 320;
-  size_t height = 240;
+  size_t width = 640;
+  size_t height = 480;
   bool burger_mode = false;
-  std::string topic("image_x");
+  std::string topic("image_front");
 
   // Force flush of the stdout buffer.
   // This ensures a correct sync of all prints
@@ -101,7 +101,7 @@ int main(int argc, char * argv[])
   }
 
   // Initialize a ROS 2 node to publish images read from the OpenCV interface to the camera.
-  auto node = rclcpp::Node::make_shared("cam2image");
+  auto node = rclcpp::Node::make_shared("cam2image_front");
   rclcpp::Logger node_logger = node->get_logger();
 
   // Set the parameters of the quality of service profile. Initialize as the default profile
@@ -149,7 +149,7 @@ int main(int argc, char * argv[])
   if (!burger_mode) {
     // Initialize OpenCV video capture stream.
     // Always open device 0.
-    cap.open("/dev/videox",cv::CAP_V4L2);
+    cap.open("/dev/videofront",cv::CAP_V4L2);
 
     // Set the width and height based on command line arguments.
     cap.set(cv::CAP_PROP_FRAME_WIDTH, static_cast<double>(width));
@@ -190,7 +190,7 @@ int main(int argc, char * argv[])
       if (show_camera) {
         cv::Mat cvframe = frame;
         // Show the image in a window called "cam2image".
-        cv::imshow("cam2image", cvframe);
+        cv::imshow("cam2image_front", cvframe);
         // Draw the image to the screen and wait 1 millisecond.
         cv::waitKey(1);
       }
