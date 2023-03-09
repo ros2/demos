8b5abbb3fbe91926489acbdcd5db0e57bcc43e3c
Bey Hao Yun <beyhy94@gmail.com>
2023-03-09 19:58:48 +0800

    Resolved flake error.
    Signed-off-by: Bey Hao Yun <beyhy94@gmail.com>

--- dummy_robot/dummy_robot_bringup/launch/dummy_robot_bringup.launch.py
+++ dummy_robot/dummy_robot_bringup/launch/dummy_robot_bringup.launch.py
@@ -16,8 +16,8 @@ import os
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
 from launch.actions import DeclareLaunchArgument
+from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import LaunchConfiguration
 
 
