b7a06c8b928702cdc42383cd733ff00cf46ed8b8
johnnyliu2886 <johnnyliu2887@gmail.com>
2023-07-31 11:25:01 +0800

    remove .obj*, add libopencv-dev back to the package.xml

--- image_tools/.obj-x86_64-linux-gnu/CMakeFiles/image_tools_custom_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/image_tools_custom_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/image_tools_custom_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
