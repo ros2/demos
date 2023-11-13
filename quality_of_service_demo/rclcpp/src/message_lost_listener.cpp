a29e65623e8ace15b8253ab33f19f3d8165c3cfe
AiVerisimilitude <133206333+AiVerisimilitude@users.noreply.github.com>
2023-11-13 23:19:47 +0100

    Explicit time conversion (#657)
    Signed-off-by: Guilherme Rodrigues <guilherme.rodrigues@ait.ac.at>

--- quality_of_service_demo/rclcpp/src/message_lost_listener.cpp
+++ quality_of_service_demo/rclcpp/src/message_lost_listener.cpp
@@ -33,7 +33,8 @@ public:
       [this](const sensor_msgs::msg::Image::ConstSharedPtr msg) -> void
       {
         rclcpp::Time now = this->get_clock()->now();
-        auto diff = now - msg->header.stamp;
+        rclcpp::Time stamp_time(msg->header.stamp, RCL_ROS_TIME);
+        auto diff = now - stamp_time;
         RCLCPP_INFO(
           this->get_logger(),
           "I heard an image. Message single trip latency: [%f]",
