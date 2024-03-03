104747b8c575e744977ec1e381b75eda22b7fc36
Chris Lalancette <clalancette@gmail.com>
2024-03-03 14:24:51 -0500

    A few uncrustify fixes for 0.78. (#667)
    Redo a couple lines of formatting to handle both 0.72
    and 0.78, and disable formatting across our one very
    large line.
    
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- demo_nodes_cpp/src/services/introspection_service.cpp
+++ demo_nodes_cpp/src/services/introspection_service.cpp
@@ -65,10 +65,10 @@ public:
   explicit IntrospectionServiceNode(const rclcpp::NodeOptions & options)
   : Node("introspection_service", options)
   {
-    auto handle_add_two_ints =
-      [this](const std::shared_ptr<rmw_request_id_t> request_header,
-        const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> request,
-        std::shared_ptr<example_interfaces::srv::AddTwoInts::Response> response) -> void
+    auto handle_add_two_ints = [this](
+      const std::shared_ptr<rmw_request_id_t> request_header,
+      const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> request,
+      std::shared_ptr<example_interfaces::srv::AddTwoInts::Response> response) -> void
       {
         (void)request_header;
         RCLCPP_INFO(
