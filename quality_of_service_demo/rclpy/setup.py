46b42abc0cee08fe34fc4373694c8786c69ed8bf
mosfet80 <10235105+mosfet80@users.noreply.github.com>
2025-07-13 11:56:29 +0200

    fix setuptools deprecations
    FIX:
    
    #UserWarning: Unknown distribution option: 'tests_require'
    
    and
    
    #SetuptoolsDeprecationWarning: License classifiers are deprecated. !!
    
    ******************************************************************************** Please consider removing the following classifiers in favor of a SPDX license expression:
    
    License :: OSI Approved :: Apache Software License
    
    See https://packaging.python.org/en/latest/guides/writing-pyproject-toml/#license for details. ********************************************************************************
    
    Signed-off-by: mosfet80 <10235105+mosfet80@users.noreply.github.com>

--- quality_of_service_demo/rclpy/setup.py
+++ quality_of_service_demo/rclpy/setup.py
@@ -18,13 +18,16 @@ setup(
     keywords=['ROS'],
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
     description='Python nodes to demonstrate ROS 2 QoS policies.',
     license='Apache License, Version 2.0',
-    tests_require=['pytest'],
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
         'console_scripts': [
             'deadline = quality_of_service_demo_py.deadline:main',
