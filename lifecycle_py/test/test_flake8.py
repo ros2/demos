62d40125134eb4fedb59b70efa336d9c634a3d0d
mohit-swaran <mohit.swaran@gmail.com>
2026-03-09 15:32:29 +0000

    Revert lifcycle_py merge
    Signed-off-by: mohit-swaran <mohit.swaran@gmail.com>

--- lifecycle_py/test/test_flake8.py
+++ lifecycle_py/test/test_flake8.py
@@ -18,7 +18,7 @@ import pytest
 
 @pytest.mark.flake8
 @pytest.mark.linter
-def test_flake8() -> None:
+def test_flake8():
     rc, errors = main_with_errors(argv=[])
     assert rc == 0, \
         'Found %d code style errors / warnings:\n' % len(errors) + \
