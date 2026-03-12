a535bb9162f5a1e958c3759bc00fb00c8b3d0b9c
mohit-swaran <mohit.swaran@gmail.com>
2026-03-12 17:43:21 +0000

    Ignore unused-ignore error for Linux #768
    Signed-off-by: mohit-swaran <mohit.swaran@gmail.com>

--- lifecycle_py/lifecycle_py/talker.py
+++ lifecycle_py/lifecycle_py/talker.py
@@ -35,7 +35,7 @@ from typing_extensions import Unpack
 class LifecycleTalker(Node):
     """Our lifecycle talker node."""
 
-    # type: ignore
+    # type: ignore[unused-ignore]
     def __init__(self, node_name: str, **kwargs: Unpack[LifecycleNodeArgs]) -> None:
         """Construct the node."""
         self._count: int = 0
