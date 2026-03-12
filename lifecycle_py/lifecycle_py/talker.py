727c1f7fc6b6ab9758ca4b03c206ccdf754ba07b
mohit-swaran <mohit.swaran@gmail.com>
2026-03-12 17:11:53 +0000

    Ignore Unpack type error for RHEL compatibility
    Signed-off-by: mohit-swaran <mohit.swaran@gmail.com>

--- lifecycle_py/lifecycle_py/talker.py
+++ lifecycle_py/lifecycle_py/talker.py
@@ -35,6 +35,7 @@ from typing_extensions import Unpack
 class LifecycleTalker(Node):
     """Our lifecycle talker node."""
 
+    # type: ignore
     def __init__(self, node_name: str, **kwargs: Unpack[LifecycleNodeArgs]) -> None:
         """Construct the node."""
         self._count: int = 0
