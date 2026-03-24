a8429e6ff67eb076b6541855ac2f0f4d22dc06df
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-03-24 22:26:15 +0100

    0.33.10

--- action_tutorials/action_tutorials_py/setup.py
+++ action_tutorials/action_tutorials_py/setup.py
@@ -4,7 +4,7 @@ package_name = 'action_tutorials_py'
 
 setup(
     name=package_name,
-    version='0.33.9',
+    version='0.33.10',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
