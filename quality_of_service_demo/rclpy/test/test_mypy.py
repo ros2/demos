5dcba084165448211e7a58c5df1e8a4addba466d
Shivam Maurya <shivammaurya14032005@gmail.com>
2026-05-22 09:41:07 +0530

    Updated test_mypy.py as per issue description
    Signed-off-by: Shivam Maurya <shivammaurya14032005@gmail.com>

--- quality_of_service_demo/rclpy/test/test_mypy.py
+++ quality_of_service_demo/rclpy/test/test_mypy.py
@@ -1,4 +1,4 @@
-# Copyright 2026 Open Source Robotics Foundation, Inc.
+# Copyright 2019 Canonical, Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,12 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest
+
+from ament_mypy.main import main
 
 
-@pytest.mark.mypy
-@pytest.mark.linter
-def test_mypy():
-    from ament_mypy.main import main
-    rc = main()
+def test_mypy() -> None:
+    rc = main(argv=['--ament-strict'])
     assert rc == 0, 'Found code style errors / warnings'
